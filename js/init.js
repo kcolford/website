75960f461e98182516df0206e40e36ca248a560b
Kieran Colford <colfordk@gmail.com>
2015-05-13 18:29:07 -0400

    fixed up indentation

--- js/init.js
+++ js/init.js
@@ -3,81 +3,82 @@
 jQuery(document).ready(function($) {
   
   // FitText Settings.
-    setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
+  setTimeout(function() {
+    $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+  }, 100);
 
 
-   // Smooth Scrolling
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+  // Smooth Scrolling
+  $('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+    var target = this.hash,
+    $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-
-
-   // Highlight our current section in nav bar
-	var sections = $("section");
-	var navigation_links = $("#nav-wrap a");
-
-	sections.waypoint({
-
-      handler: function(event, direction) {
-
-		   var active_section;
-
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-		offset: '35%'
-
-	});
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
 
 
-   // Make sure the header background is correctly framed.
-   $('header').css({ 'height': $(window).height() });
-   $(window).on('resize', function() {
-        $('header').css({ 'height': $(window).height() });
-        $('body').css({ 'width': $(window).width() })
-   });
+  // Highlight our current section in nav bar
+  var sections = $("section");
+  var navigation_links = $("#nav-wrap a");
+
+  sections.waypoint({
+
+    handler: function(event, direction) {
+
+      var active_section;
+
+      active_section = $(this);
+      if (direction === "up") active_section = active_section.prev();
+
+      var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+      navigation_links.parent().removeClass("current");
+      active_link.parent().addClass("current");
+
+    },
+    offset: '35%'
+
+  });
 
 
-   // Fade in/out nav bar.
-   $(window).on('scroll', function() {
+  // Make sure the header background is correctly framed.
+  $('header').css({ 'height': $(window).height() });
+  $(window).on('resize', function() {
+    $('header').css({ 'height': $(window).height() });
+    $('body').css({ 'width': $(window).width() });
+  });
 
-		var h = $('header').height();
-		var y = $(window).scrollTop();
-      var nav = $('#nav-wrap');
 
-	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
-	      nav.fadeOut('fast');
-	   }
-      else {
-         if (y < h*.20) {
-            nav.removeClass('opaque').fadeIn('fast');
-         }
-         else {
-            nav.addClass('opaque').fadeIn('fast');
-         }
+  // Fade in/out nav bar.
+  $(window).on('scroll', function() {
+
+    var h = $('header').height();
+    var y = $(window).scrollTop();
+    var nav = $('#nav-wrap');
+
+    if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+      nav.fadeOut('fast');
+    }
+    else {
+      if (y < h*.20) {
+        nav.removeClass('opaque').fadeIn('fast');
       }
+      else {
+        nav.addClass('opaque').fadeIn('fast');
+      }
+    }
 
-	});
+  });
 
-   // Copy social media links to bottom of page.
-   $("header .social li").clone().appendTo("#bottom-social");
+
+  // Copy social media links to bottom of page.
+  $("header .social li").clone().appendTo("#bottom-social");
 
 });
 
