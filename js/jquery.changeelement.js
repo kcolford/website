723224346c59bf0d60917c26305fa9e13a86f6e5
Kieran Colford <colfordk@gmail.com>
2015-05-23 14:37:00 -0400

    added jquery element changer

---
+++ js/jquery.changeelement.js
@@ -0,0 +1,18 @@
+// jQuery plugin to change the type of an element.
+
+// Courtesy of
+// http://stackoverflow.com/questions/8584098/how-to-change-an-element-type-using-jquery#answer-8584217
+
+(function($) {
+    $.fn.changeElementType = function(newType) {
+        var attrs = {};
+
+        $.each(this[0].attributes, function(idx, attr) {
+            attrs[attr.nodeName] = attr.nodeValue;
+        });
+
+        this.replaceWith(function() {
+            return $("<" + newType + "/>", attrs).append($(this).contents());
+        });
+    };
+})(jQuery);
