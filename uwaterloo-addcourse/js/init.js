63073af54e69cc914f3119b7433e60473c4a4320
Kieran Colford <kieran@kcolford.com>
2017-01-31 13:26:38 -0500

    cleanup

--- uwaterloo-addcourse/js/init.js
+++
@@ -1,8 +0,0 @@
-// Configuration for the addcourse page.
-
-jQuery(document).ready(function ($) {
-  $("body > div.section").changeElementType("section");
-  $("body > :not(section)").wrapAll("<header></header>");
-  $("section,header").wrapInner("<div class='row'></div>");
-  $("section#credits").changeElementType("footer");
-});
